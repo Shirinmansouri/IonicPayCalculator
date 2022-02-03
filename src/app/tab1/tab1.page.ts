ae96bebdfbc286fe03c253cfa8a8a8aed2798870
Shirin <shirin.mansori@gmail.com>
2022-02-03 18:40:58 -0500

    routing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+  NumberOfHoursWorked : number
+  HourlyRate : number
+  regularPay : number
+  overtimePay : number
+  totalPay : number
+  tax : number
 
+  CalculatePay() {
+   if (this.NumberOfHoursWorked<=40)
+   {
+     this.regularPay = this.NumberOfHoursWorked * this.HourlyRate
+     this.totalPay = this.regularPay
+     this.overtimePay = 0.0
+   }
+   else
+   {
+    this.regularPay  = 40 * this.HourlyRate
+    this.overtimePay = (this.NumberOfHoursWorked - 40) * this.HourlyRate *1.5 
+    this.totalPay = this.overtimePay+ this.regularPay 
+   }
+   this.tax=  this.totalPay  * 0.18 
+ 
+ }
+    
+  
 }
